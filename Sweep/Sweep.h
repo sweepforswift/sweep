e49a292ec82c7607786b7875e4833262bd754fe5
Michael Smith <mdsf3f@mail.missouri.edu>
2017-02-24 22:02:02 -0600

    Initial Project push

---
+++ Sweep/Sweep.h
@@ -0,0 +1,19 @@
+//
+//  Sweep.h
+//  Sweep
+//
+//  Created by Michael Smith on 2/24/17.
+//  Copyright © 2017 Michael Smith Jr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sweep.
+FOUNDATION_EXPORT double SweepVersionNumber;
+
+//! Project version string for Sweep.
+FOUNDATION_EXPORT const unsigned char SweepVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sweep/PublicHeader.h>
+
+
