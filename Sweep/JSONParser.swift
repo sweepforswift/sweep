6c98d052a03d6a5529d784cb687f9bd2f0a018e2
Michael Smith <mdsf3f@mail.missouri.edu>
2017-02-25 17:09:20 -0600

    Added Doc blocks to all functions

--- Sweep/JSONParser.swift
+++ Sweep/JSONParser.swift
@@ -9,10 +9,18 @@
 import Foundation
 
 public protocol JSONAble{
+    /**
+     Allows the object to be constructed via JSON
+     - parameter json: A JSON dictionary for the object
+    */
     init(json: [String:Any])
 }
 
 public extension JSONAble{
+    /**
+     Converts the JSONAble object into a properly formatted JSON string.
+     - returns: A properly formatted JSON string representing the object.
+    */
     public func toJSON() -> String{
         var jsonString = "{"
         let m = Mirror(reflecting: self)
@@ -32,6 +40,10 @@ public extension JSONAble{
 }
 
 public extension Sequence where Iterator.Element: JSONAble{
+    /**
+     Converts the array of JSONAble items into a proper JSON string
+     - returns: A properly formatted JSON string representing the array of objects
+    */
     public func toJSON() -> String{
         var jsonString = "["
         for item in self{
@@ -46,6 +58,11 @@ public extension Sequence where Iterator.Element: JSONAble{
 
 
 public class JSONParser{
+    /**
+     Converts the given JSON string into either a Dictionary or Array
+     - parameter jsonString: The JSON string to be converted
+     - returns: Either a Dictionary or Array based upon the variable the return is assigned to
+    */
     public static func parse<T>(jsonString: String) -> T?{
         let data = jsonString.data(using: .utf8)
         let json:T? = self.parse(jsonData: data!)
@@ -56,6 +73,11 @@ public class JSONParser{
         return result
     }
     
+    /**
+     Converts the given JSON data into either a Dictionary or Array
+     - parameter jsonData: The JSON data to be converted
+     - returns: Either a Dictionary or Array based upon the variable the return is assigned to
+    */
     public static func parse<T>(jsonData: Data) -> T?{
         let json = try? JSONSerialization.jsonObject(with: jsonData, options: [])
         
@@ -65,6 +87,12 @@ public class JSONParser{
         return result
     }
     
+    /**
+     Converts the given JSON data into an array of classes based upon variable the return is assigned to.
+     The class in the array must conform to the JSONAble protocol.
+     - parameter jsonData: The JSON data to be converted
+     - returns: An array of objects that conform to the JSONAble protocol
+     */
     public static func parse<T>(toObject jsonData:Data) -> [T]? where T:JSONAble{
         let json = try? JSONSerialization.jsonObject(with: jsonData, options: [])
         
@@ -83,6 +111,12 @@ public class JSONParser{
         return nil
     }
     
+    /**
+     Converts the given JSON string into an array of classes based upon variable the return is assigned to.
+     The class in the array must conform to the JSONAble protocol.
+     - parameter jsonData: The JSON data to be converted
+     - returns: An array of objects that conform to the JSONAble protocol
+     */
     public static func parse<T>(toObject jsonString: String) -> [T]? where T:JSONAble{
         let data = jsonString.data(using: .utf8)
         return self.parse(toObject: data!)
